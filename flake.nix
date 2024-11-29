424d49efa0f6f6a893ef62c5c4ef0d868a30788b
Aubrey <me@aubrey.codes>
2024-11-29 23:19:53 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,27 +8,20 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, agenix }: 
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          agenix.nixosModules.default
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
-            environment.systemPackages = [ agenix.packages.${system}.default ];
-          }
-        ];
-        specialArgs = { inherit pkgs; };
-      };
+  outputs = { self, nixpkgs, home-manager, agenix }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        agenix.nixosModules.default
+        ({pkgs, ...}: {
+          nixpkgs.config.allowUnfree = true;
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          environment.systemPackages = [ agenix.packages.${pkgs.system}.default ];
+        })
+      ];
     };
+  };
 }
